ef976ccad210a8ae8fb64fa4028bb51b817574c5
Max <maxhard243@gmail.com>
2023-03-19 23:19:53 +0300

    refactor: add oop and modules

--- scripts/card.js
+++ scripts/card.js
@@ -1,26 +1,38 @@
-export const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+ import {openImagePopup} from "./index.js"
+ 
+ export class Card {
+  constructor(data, cardTemplate) {
+    this._data = data;
+    this._cardTemplate = cardTemplate;
   }
-]
+
+  _getTemplate() {
+    const cardElement = document.querySelector(this._cardTemplate).content.querySelector(".card").cloneNode(true);
+    return cardElement;
+  }
+
+  _changeLikeButton(evt) {
+    evt.target.classList.toggle("card__like-button_active");
+  }
+
+  _deleteCard(evt) {
+    evt.target.closest(".card").remove();
+  }
+
+  _setEventListeners() {
+    this._element.querySelector(".card__like-button").addEventListener("click", evt => {this._changeLikeButton(evt)});
+    this._element.querySelector(".card__trash-button").addEventListener("click", evt => {this._deleteCard(evt)});
+    this._element.querySelector(".card__image").addEventListener("click", () => {openImagePopup(this._data)});
+  }
+
+  createCard() {
+    this._element = this._getTemplate();
+    const cardImage = this._element.querySelector(".card__image");
+
+    this._setEventListeners();
+    cardImage.src = this._data.link;
+    cardImage.alt = this._data.name;
+    this._element.querySelector(".card__title").textContent = this._data.name;
+    return this._element
+  }
+}
