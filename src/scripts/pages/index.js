9e5f61fe7fa71244ece74e01aed49ba5812285db
Maxim <maxhard243@gmail.com>
2023-04-08 16:08:10 +0300

    feat: add webpack build

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -5,6 +5,7 @@ import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
+import "../../pages/index.css"
 
 export {openImagePopup}
 
